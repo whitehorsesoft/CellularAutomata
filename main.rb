25a9704b277c73fd9e80ca21a9a39ab348ddb70a
devops <devops@whitehorsesoft.com>
2018-12-28 07:00:30 -0600

    simplified

--- main.rb
+++ main.rb
@@ -1,8 +1,8 @@
 # pattern is a tribit (3 bits) in string form, rule is octet as base-10 in int form
 def f1(pattern, rule)
   # convert rule to bit array, then to array of strings, with each string being "0" or "1"
-  match_arr = sprintf("%0.8b", rule).chars.map{|c| c == "1" ? true : false}
-  # reverse rule's bit array, then match pattern to rule - pattern is 3 bits
+  match_arr = sprintf("%0.8b", rule).chars
+  # reverse rule, then match pattern to rule - pattern is 3 bits
   result = match_arr.reverse[pattern.to_i(2)]
   return result
 end
@@ -16,16 +16,20 @@ def transform(line, rule)
     pattern[2] = (i == line.length - 1)? line.first : line[i + 1]
     result[i] = f1(pattern.join, rule)
   end
-  result.map{|r| r ? "1" : "0"}
+  result
 end
 
 def go
   line = Array.new(60, "0")
   # initial condition
   line[29] = "1"
-  puts line.join
+  pr line
   (0..19).each do |i|
     line = transform(line, 30)
-    puts line.join
+    pr line
   end
+end
+
+def pr(line)
+  puts line.map{|c| c == "1" ? "█" : " "}.join
 end
