115fe7ff92c6befe64acea85465f0912356bc656
devops <devops@whitehorsesoft.com>
2018-12-29 07:23:43 -0600

    bitwise operators and using refine

--- main.rb
+++ main.rb
@@ -1,35 +1,34 @@
-# pattern is a tribit (3 bits) in string form, rule is octet as base-10 in int form
-def f1(pattern, rule)
-  # convert rule to bit array, then to array of strings, with each string being "0" or "1"
-  match_arr = sprintf("%0.8b", rule).chars
-  # reverse rule, then match pattern to rule - pattern is 3 bits
-  result = match_arr.reverse[pattern.to_i(2)]
-  return result
-end
-
-def transform(line, rule)
-  result = Array.new(line.length, "0")
-  line.each_with_index do |b, i|
-    pattern = Array.new(3)
-    pattern[0] = i == 0 ? line.last : line[i - 1]
-    pattern[1] = line[i]
-    pattern[2] = (i == line.length - 1)? line.first : line[i + 1]
-    result[i] = f1(pattern.join, rule)
+module CellularAutomata
+  refine Array do
+    def transform(rule)
+      result = Array.new(self.length, false)
+      self.each_with_index do |b, i|
+        pattern = Array.new(3)
+        pattern[0] = i == 0 ? self.last : self[i - 1]
+        pattern[1] = self[i]
+        pattern[2] = (i == self.length - 1)? self.first : self[i + 1]
+        result[i] = rule[pattern[0] << 2 | pattern[1] << 1 | pattern[2]]
+      end
+      result
+    end
   end
-  result
 end
 
-def go
-  line = Array.new(60, "0")
-  # initial condition
-  line[29] = "1"
-  pr line
-  (0..19).each do |i|
-    line = transform(line, 30)
+using CellularAutomata
+
+class Go
+  def initialize
+    line = Array.new(60, 0b0)
+    # initial condition
+    line[29] = 0b1
     pr line
+    (0..19).each do |i|
+      line = line.transform(30)
+      pr line
+    end
   end
-end
 
-def pr(line)
-  puts line.map{|c| c == "1" ? "█" : " "}.join
+  def pr(line)
+    puts line.map{|c| c == 0b1 ? "█" : " "}.join
+  end
 end
